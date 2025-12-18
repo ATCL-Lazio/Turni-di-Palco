57303a64524e33144ddfa788c4601d445e7a4d7c
Heartran <51830223+Heartran@users.noreply.github.com>
2025-12-18 17:17:01 +0100

    Restore mobile folder from e23b07

--- apps/mobile/src/main.tsx
+++ apps/mobile/src/main.tsx
@@ -1,7 +1,19 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "../../../shared/styles/main.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+// On the dev server (5173) keep SW caches out of the way to avoid stale bundles.
+const isDevServer = window.location.port === "5173";
+
+if ("serviceWorker" in navigator && isDevServer) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister().catch(() => undefined));
+  });
+
+  if ("caches" in window) {
+    caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))).catch(() => undefined));
+  }
+}
+
+createRoot(document.getElementById("root")!).render(<App />);
