be98706ab76deb685adcb0d0f835866538793cff
Heartran <51830223+Heartran@users.noreply.github.com>
2026-02-24 14:54:28 +0100

    Fix coverage configuration for Vitest

--- apps/pwa/vitest.config.ts
+++ apps/pwa/vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     setupFiles: "src/test/setup.ts",
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
+      reporter: ["text", "json", "html", "lcov"],
       reportsDirectory: "./coverage",
       exclude: [
         "node_modules/",
